ec31fe8dfafae30138b43846d0826d989a15433b
Jeszpy <hleb.lukashonak@gmail.com>
2022-11-09 01:36:00 +0300

    rerun gh action

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ const swaggerConfig = new DocumentBuilder()
 //TODO: create setAppPipes-Guards() func
 
 async function bootstrap() {
+  const a = process.env;
+  console.log(a);
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // --- Pipes ---
